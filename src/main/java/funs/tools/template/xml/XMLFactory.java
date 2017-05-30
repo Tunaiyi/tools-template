98e32018964a2fb74965a4860c812bdc315224ae
KGTny <kgtny@KGTnydeMacBook-Pro.local>
2017-05-30 13:17:11 +0800

    change : utf-8

--- src/main/java/funs/tools/template/xml/XMLFactory.java
+++ src/main/java/funs/tools/template/xml/XMLFactory.java
@@ -1,33 +1,37 @@
 package funs.tools.template.xml;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-
 public class XMLFactory {
 
-	public static XMLTable createTable(InputStream input) {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
-		StringBuilder builder = new StringBuilder();
-		String line = null;
-		try {
-			while ((line = bufferedReader.readLine()) != null) {
-				builder.append(line.trim());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		XMLMapConverter xmlMapConverter = new XMLMapConverter();
-		XStream xStream = new XStream(new DomDriver());
-		xStream.alias("table", XMLTable.class);
-		xStream.registerLocalConverter(XMLTable.class, "attributeMap", xmlMapConverter);
-		XMLTable table = (XMLTable) xStream.fromXML(builder.toString());
-		//		System.out.println(JSONUtils.toJson(table));
-		return table;
-	}
+    public static XMLTable createTable(InputStream input) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+                    builder.append(line.trim());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            XMLMapConverter xmlMapConverter = new XMLMapConverter();
+            XStream xStream = new XStream(new DomDriver());
+            xStream.alias("table", XMLTable.class);
+            xStream.registerLocalConverter(XMLTable.class, "attributeMap", xmlMapConverter);
+            //		System.out.println(JSONUtils.toJson(table));
+            return (XMLTable) xStream.fromXML(builder.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
